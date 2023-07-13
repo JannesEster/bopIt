36dba2de32180347cae80adc7fd2b367d4b0a211
JannesEster <104826287+JannesEster@users.noreply.github.com>
2023-07-14 00:11:35 +0800

    Added flashing animmation and sound to buttons

--- game.js
+++ game.js
@@ -7,5 +7,8 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio('sounds/' + randomChosenColour +'.mp3');
+    audio.play();
     console.log(gamePattern);
 }
