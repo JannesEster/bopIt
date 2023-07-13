93e83ca6be5c6d7c7131b7d5215bb2c0ce97738b
JannesEster <104826287+JannesEster@users.noreply.github.com>
2023-07-14 01:02:34 +0800

    added click function and animation with array logging

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
 
 
@@ -8,7 +9,29 @@ function nextSequence() {
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio('sounds/' + randomChosenColour +'.mp3');
-    audio.play();
+    playSound(randomChosenColour);
     console.log(gamePattern);
 }
+
+$(".btn").on("click",function(){
+    var userChosenColour = this.id;
+    userClickedPattern.push(this.id);
+    playSound(this.id);
+    animatePress(this.id);
+    console.log(userClickedPattern);
+});
+
+function playSound(name) {
+    var audio = new Audio('sounds/' + name +'.mp3');
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $('#'+ currentColour).addClass("pressed");
+    setTimeout(function(){
+        $('#'+ currentColour).removeClass("pressed");
+    },100);
+
+}
+
+
